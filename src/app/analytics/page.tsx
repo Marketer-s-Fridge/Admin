748e9aa9119ffd7fb4ab96cd4d924e99f666bc1b
zzizi <zzi_zi@naver.com>
2025-06-29 19:06:47 +0900

    feature: 통계 및 분석 목업 구현, 표 내용 변경

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -5,90 +5,9 @@ import AdminHeader from "@/components/adminHeader";
 import AdminCategoryBar from "@/components/adminCategoryBar";
 import Pagination from "@/components/pagination";
 import CustomDropdown from "@/components/customDropdown";
-import { FiPaperclip } from "react-icons/fi";
 import AdminSearchInput from "@/components/adminSearchInput";
 import Image from "next/image";
 import DateRangePickerModal from "@/components/dateRangePickerModal";
-
-interface InquiryItem {
-  id: number;
-  name: string;
-  email: string;
-  type: string;
-  date: string;
-  status: "답변 완료" | "답변 임시저장" | "접수됨" | "스팸/무효";
-  responder: string;
-  hasAttachment?: boolean;
-}
-
-const sampleData: InquiryItem[] = [
-  {
-    id: 7,
-    name: "Sohn678",
-    email: "sohn678@gmail.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "답변 완료",
-    responder: "Admin12",
-    hasAttachment: true,
-  },
-  {
-    id: 6,
-    name: "luckyseven",
-    email: "ahn123@gmail.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "답변 임시저장",
-    responder: "Admin12",
-  },
-  {
-    id: 5,
-    name: "saraminjoa",
-    email: "park987@naver.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "접수됨",
-    responder: "Admin12",
-  },
-  {
-    id: 4,
-    name: "vividsun44",
-    email: "jung456@gmail.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "스팸/무효",
-    responder: "Admin12",
-  },
-  {
-    id: 3,
-    name: "springbloom57",
-    email: "lee@gmail.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "답변 완료",
-    responder: "Admin12",
-    hasAttachment: true,
-  },
-  {
-    id: 2,
-    name: "banjjak",
-    email: "kim012@kakao.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "답변 임시저장",
-    responder: "Admin12",
-    hasAttachment: true,
-  },
-  {
-    id: 1,
-    name: "winterstorm",
-    email: "baek789@naver.com",
-    type: "기술적 문제",
-    date: "2025/05/10",
-    status: "스팸/무효",
-    responder: "Admin12",
-  },
-];
 
 const AnalyticsPage = () => {
   const [search, setSearch] = useState("");
@@ -137,56 +56,36 @@ const AnalyticsPage = () => {
         </div>
 
         {/* 테이블 헤더 */}
-        <div className="grid grid-cols-[1fr_2.2fr_3.5fr_2fr_2fr_2fr_2fr] py-2 border-y-[2px] border-gray-500 font-bold text-sm text-gray-800">
-          <div className="text-center">문의번호</div>
-          <div className="text-center">문의자</div>
-          <div className="text-center">이메일</div>
-          <div className="text-center">문의 유형</div>
-          <div className="text-center">문의 날짜</div>
-          <div className="text-center">처리 상태</div>
-          <div className="text-center">답변자</div>
+        <div className="grid grid-cols-[0.7fr_4fr_2fr_2fr_2fr_2fr] py-2 border-y-[2px] border-gray-500 font-bold text-sm text-gray-800">
+          <div className="text-center">번호</div>
+          <div className="text-center">콘텐츠</div>
+          <div className="text-center">업로드 날짜</div>
+          <div className="text-center">조회수</div>
+          <div className="text-center">클릭수</div>
+          <div className="text-center">반응률</div>
         </div>
 
         {/* 테이블 데이터 */}
-        {sampleData.map((item) => (
+        {Array.from({ length: 8 }).map((_, i) => (
           <div
-            key={item.id}
-            className="grid grid-cols-[1fr_2.5fr_3.2fr_2fr_2fr_2fr_2fr] py-3 items-center text-sm text-gray-700 "
+            key={i}
+            className="grid grid-cols-[0.7fr_4fr_2fr_2fr_2fr_2fr] py-4 items-center text-sm text-gray-700"
           >
-            <div className="flex justify-center items-center font-semibold ">
-              {item.id}
+            <div className="flex justify-center font-semibold">{15}</div>
+            <div className="flex items-center gap-4">
+              <img
+                src="/images/Category-1.jpg"
+                alt="콘텐츠 썸네일"
+                className="w-10 aspect-[3/4] rounded object-cover"
+              />
+              <span className="font-semibold">
+                뭐라고? 쿠션이 40가지나 된다고?!
+              </span>
             </div>
-
-            <div className="flex justify-start items-center gap-1 mx-9">
-              {item.hasAttachment && (
-                <FiPaperclip className="text-gray-400 w-4 h-4" />
-              )}
-              {item.name}
-            </div>
-
-            <div className="flex justify-start items-center pl-12">
-              {item.email}
-            </div>
-            <div className="flex justify-center items-center ">{item.type}</div>
-            <div className="flex justify-center items-center">{item.date}</div>
-
-            <div
-              className={`flex justify-start items-center font-semibold pl-10 ${
-                item.status === "답변 완료"
-                  ? "text-green-600"
-                  : item.status === "답변 임시저장"
-                  ? "text-orange-400"
-                  : item.status === "접수됨"
-                  ? "text-blue-500"
-                  : "text-gray-500"
-              }`}
-            >
-              {item.status}
-            </div>
-
-            <div className="flex justify-center items-center">
-              {item.responder}
-            </div>
+            <div className="text-center">2025/05/10</div>
+            <div className="text-center">4,234</div>
+            <div className="text-center">245</div>
+            <div className="text-center">12.6%</div>
           </div>
         ))}
 
