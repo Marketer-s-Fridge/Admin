9e31390c97f9c5b0e039c7f3e5d4f98af24eb579
zzizi <zzi_zi@naver.com>
2025-06-29 18:47:09 +0900

    fix: 업로드 페이지 상태 모달 스타일 수정

--- src/components/statusSelectModal.tsx
+++ src/components/statusSelectModal.tsx
@@ -23,8 +23,8 @@ const StatusSelectModal: React.FC<StatusSelectModalProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black/30 flex justify-center items-center z-50">
-      <div className="bg-white w-[400px] rounded-xl p-9">
-        <h2 className="text-xl font-bold mb-6">작업 상태를 선택해주세요.</h2>
+      <div className="bg-white w-[400px] rounded-xl p-7">
+        <h2 className="text-2xl font-bold mb-8">작업 상태를 선택해주세요.</h2>
 
         <div className="flex flex-col gap-6 mb-6">
           {statusOptions.map((status) => (
@@ -33,12 +33,24 @@ const StatusSelectModal: React.FC<StatusSelectModalProps> = ({
               className="flex justify-between items-center cursor-pointer"
             >
               <span className="text-gray-800">{status}</span>
-              <input
-                type="checkbox"
-                checked={selected === status}
-                onChange={() => setSelected(status)}
-                className="w-5 h-5 accent-gray-600"
-              />
+              <label className="relative w-5 h-5">
+                <input
+                  type="checkbox"
+                  checked={selected === status}
+                  onChange={() => setSelected(status)}
+                  className="appearance-none w-5 h-5 border border-gray-300 rounded-sm 
+               checked:bg-[#555555] checked:border-gray-300 
+               hover:border-gray-600 hover:border-2"
+                />
+                {/* 체크 시 아이콘 이미지 띄우기 */}
+                {selected === status && (
+                  <img
+                    src="/icons/check.png"
+                    alt="체크"
+                    className="absolute inset-0 w-3 h-3 m-auto pointer-events-none"
+                  />
+                )}
+              </label>
             </label>
           ))}
         </div>
