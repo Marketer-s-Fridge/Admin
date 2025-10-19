8cafe52d10aacb80238bb3b91048e15728661b7c
zzizi <zzi_zi@naver.com>
2025-10-19 23:15:13 +0900

    chore: clear cache before build for mf-admin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 // next.config.js
 const nextConfig = {
   output: "standalone",
+  basePath: "/admin",      // ✅ 라우터 기준 경로
   assetPrefix: "/admin", // ✅ 정적 리소스 prefix 맞춰줌
   eslint: { ignoreDuringBuilds: true },
 };
