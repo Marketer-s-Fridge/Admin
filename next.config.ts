a42d162ffe633cdafbc6e5a661a70aafa0fbf7cf
zzizi <zzi_zi@naver.com>
2025-10-19 22:32:57 +0900

    chore: base url 변경

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "standalone",
-  eslint: { ignoreDuringBuilds: true },
+import type { NextConfig } from "next";
 
-  async rewrites() {
-    return [
-      // ✅ auth 요청은 /api 없이 바로 백엔드로
-      {
-        source: "/auth/:path*",
-        destination:
-          process.env.NODE_ENV === "production"
-            ? "http://mf-api:8080/auth/:path*"
-            : "http://localhost:8081/auth/:path*",
-      },
-      // ✅ 나머지 /api 요청
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "production"
-            ? "http://mf-api:8080/api/:path*"
-            : "http://localhost:8081/api/:path*",
-      },
-    ];
-  },
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "standalone",
 };
 
 export default nextConfig;
